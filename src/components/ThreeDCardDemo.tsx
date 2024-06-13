e0d1de29bdb525fb4e8753b656e316a3a61691b7
Abhijeet Kumar <133308751+403ABHIJEET@users.noreply.github.com>
2024-06-13 09:20:11 +0530

    components

---
+++ src/components/ThreeDCardDemo.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
+
+interface myProps {
+  source: string,
+  title: string,
+  description: string
+}
+
+export function ThreeDCardDemo({ source, title, description }: myProps) {
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-black relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-white dark:text-white"
+        >
+          {title}
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-white text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          {description}
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <img
+            src={source}
+            height="1000"
+            width="1000"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+            alt="thumbnail"
+          />
+        </CardItem>
+        <div className="flex justify-center items-center mt-20">
+          <CardItem
+            translateZ={20}
+            as="button"
+            className="px-4 py-2 rounded-xl bg-black dark:bg-white dark:text-black text-white text-xs font-bold border hover:border-none hover:bg-white hover:text-black"
+          >
+            Visit Now
+          </CardItem>
+        </div>
+      </CardBody>
+    </CardContainer>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,50 @@
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        orbit: "orbit calc(var(--duration)*1s) linear infinite",
+      },
+      keyframes: {
+        orbit: {
+            "0%": {
+                transform: "rotate(0deg) translateY(calc(var(--radius) * 1px)) rotate(0deg)",
+            },
+            "100%": {
+                transform: "rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)",
+            },
+        },
+      },
+    },
+  },
+};
+
+
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    // rest of the code
+  },
+  plugins: [addVariablesForColors],
+};
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
 
 const config: Config = {
   content: [
@@ -18,3 +64,7 @@ const config: Config = {
   plugins: [],
 };
 export default config;
+
+
+
+
